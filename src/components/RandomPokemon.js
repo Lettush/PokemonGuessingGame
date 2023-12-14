739aec9bd44abf0f692fc75b7911343b7c6b0e86
Lettush <jaredneilhortaleza11@gmail.com>
2023-12-14 14:24:22 +0800

    added basic guessing gameplay

--- src/components/RandomPokemon.js
+++ src/components/RandomPokemon.js
@@ -4,12 +4,13 @@ const RandomPokemon = () => {
   const [pokemonName, setPokemonName] = useState();
   const [pokemonTypes, setPokemonTypes] = useState();
   const [pokemonImage, setPokemonImage] = useState();
+  const [score, setScore] = useState(0);
 
   const generateRandom = async () => {
     const response = await fetch(
       "https://pokeapi.co/api/v2/pokemon-species/?limit=0"
     );
-    const totalPokemon = await response.json()
+    const totalPokemon = await response.json();
     const randomNumber = Math.floor(Math.random() * totalPokemon.count);
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
@@ -17,10 +18,19 @@ const RandomPokemon = () => {
       .then((data) => {
         setPokemonName(data.name);
         setPokemonTypes(data.types);
-        setPokemonImage(data["sprites"]["other"]["official-artwork"]["front_default"]);
-        console.log(data);
+        setPokemonImage(
+          data["sprites"]["other"]["official-artwork"]["front_default"]
+        );
       })
       .catch((e) => console.error(e.message));
+  };
+
+  const checkAnswer = (e) => {
+    e.preventDefault();
+    const answer = document.forms["guessingForm"]
+    if (answer.elements["answer"].value.toLowerCase() === pokemonName.toLowerCase()) {
+      setScore(score + 1);
+    }
   };
 
   useEffect(() => {
@@ -30,12 +40,29 @@ const RandomPokemon = () => {
   return (
     <div>
       {pokemonTypes && (
-        <div>
-          <h1>{pokemonName}</h1>
-          {pokemonTypes.map((type) => {
-            return <p key={type.type.name}>{type.type.name}</p>;
-          })}
-          <img src={pokemonImage} alt={pokemonName} />
+        <div className="container">
+          <img src={pokemonImage} alt={pokemonName} className="pokemonImage" />
+          <img
+            src={window.location.origin + "/img/shadow.png"}
+            alt="shadow"
+            className="shadow"
+          />
+          {/* <h1 className="pokemonName">{pokemonName}</h1> */}
+          <p>
+            {pokemonTypes.map((type) => {
+              return (
+                <span className={type.type.name + " type"} key={type.type.name}>
+                  {type.type.name.toUpperCase()}
+                </span>
+              );
+            })}
+          </p>
+          <h1 id="score">Score: {score}</h1>
+          <form name="guessingForm" onSubmit={checkAnswer}>
+            <label htmlFor="answer">Guess the Pokemon!</label><br />
+            <input type="text" name="answer" id="answer"/><br />
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       )}
     </div>
